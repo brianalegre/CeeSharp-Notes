6c8948f2a5bda55d36ca48ace4605e7baf798a8d
Brian Alegre <brialegre@yahoo.com>
2022-10-24 22:16:31 -0700

    Added Notes: OverloadingConstructors

---
+++ OverloadingConstructors/Program.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace BasicClasses
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Forest f = new Forest("Congo", "Tropical");
+      f.Trees = 0;
+
+      Console.WriteLine(f.Name);
+      Console.WriteLine(f.Biome);
+
+      // Call Second Constructor
+      Forest F = new Forest("Rendlesham");
+
+      // Print Biome
+      Console.WriteLine(f.Biome);
+    }
+  }
+}
