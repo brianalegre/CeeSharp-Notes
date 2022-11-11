48ba950a5822e1219baa0d31bf0bc2c2c3ebb6d3
Brian Alegre <brialegre@yahoo.com>
2022-11-10 21:20:32 -0800

    Added Notes: CountAndContains

---
+++ ListsAndLINQ/CountAndContains/Program.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+
+namespace LearnLists
+{
+  class Program
+  {
+    static void Main()
+    {
+      List<double> marathons = new List<double>
+      {
+        144.07,
+        143.12,
+        146.73,
+        146.33
+      };
+
+      // Print marathons with the Count Method
+      Console.WriteLine(marathons.Count);
+
+      // Add a value to the marathon list
+      marathons.Add(143.23);
+
+      // Check if value was added
+      // Print to the console
+      Console.WriteLine(marathons.Contains(143.23));
+
+      // Check marathons with Count Method again
+      // Checking to see if value increased after the add
+      Console.WriteLine(marathons.Count);
+      
+
+    }
+  }
+}
---
+++ ListsAndLINQ/CountAndContains/instructions.md
@@ -0,0 +1,1 @@
+We won’t cover everything about object initialization in this lesson, but you do need to recognize and use it.
