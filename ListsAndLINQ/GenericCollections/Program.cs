b311cf6e8239cfee5626065a1d41904fdcaa3b2e
Brian Alegre <brialegre@yahoo.com>
2022-11-11 21:31:43 -0800

    Added Notes: GenericCollections

---
+++ ListsAndLINQ/GenericCollections/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace LearnLists
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Create Three Empty Lists
+            List<bool> b = new List<bool>();
+            List<Random> r = new List<Random>();
+            List<IServiceProvider> i = new List<IServiceProvider>();
+
+        }
+    }
+}
